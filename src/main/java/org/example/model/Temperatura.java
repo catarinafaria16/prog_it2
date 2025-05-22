b2618d481a6f2cec960e4dfbd94b3df7c00ff832
mariaclara0609 <up202406088@up.pt>
2025-05-22 10:20:29 +0100

    Desenvolvimento das classes Saturacao e Temperatura

--- src/main/java/org/example/model/Temperatura.java
+++ src/main/java/org/example/model/Temperatura.java
@@ -1,4 +1,25 @@
 package org.example.model;
 
-public class Temperatura {
+import org.example.utils.Data;
+
+public class Temperatura extends Medida {
+    private double temperatura;
+
+    public Temperatura(Data dataRegisto, double temperatura, Paciente paciente, ProfissionalSaude profissionalSaude) {
+        super(dataRegisto, paciente, profissionalSaude);
+        this.temperatura = temperatura;
+    }
+
+    public double getTemperatura() {
+        return temperatura;
+    }
+
+    public void setTemperatura(double temperatura) {
+        this.temperatura = temperatura;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nTemperatura: " + temperatura;
+    }
 }

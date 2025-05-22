84284507606d3a886cae94a180283a7c8a81890b
mariaclara0609 <up202406088@up.pt>
2025-05-22 09:42:22 +0100

    Desenvolvimento da class Pessoa.

--- src/main/java/org/example/model/Pessoa.java
+++ src/main/java/org/example/model/Pessoa.java
@@ -1,4 +1,38 @@
 package org.example.model;
 
+import org.example.utils.Data;
+
 public abstract class Pessoa {
+    protected int id;
+    protected String nome;
+    protected char sexo;
+    protected Data dataNascimento;
+
+    public Pessoa(int id, String nome, char sexo, Data dataNascimento) {
+        this.id = id;
+        this.nome = nome;
+        this.sexo = sexo;
+        this.dataNascimento = new Data(dataNascimento);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public Data getDataNascimento() {
+        return dataNascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "id=" + id + ", nome='" + nome + '\'' + ", sexo=" + sexo + ", dataNascimento=" + dataNascimento + '}';
+    }
 }

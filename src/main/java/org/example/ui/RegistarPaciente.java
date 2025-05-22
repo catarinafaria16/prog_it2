047e68ca5d58c345813e47175a3384f59aa4f762
catarinafaria16 <up202405128@up.pt>
2025-05-22 17:32:34 +0100

    Desenvolvimento da class RegistarPaciente

---
+++ src/main/java/org/example/ui/RegistarPaciente.java
@@ -0,0 +1,44 @@
+package org.example.ui;
+
+import org.example.model.Hospital;
+import org.example.model.Paciente;
+import org.example.utils.Data;
+import org.example.utils.Utils;
+
+public class RegistarPaciente{
+    private Hospital hospital;
+
+    public RegistarPaciente(Hospital hospital) {
+        this.hospital = hospital;
+    }
+
+    public void run() {
+        System.out.println("Novo Paciente:");
+
+        Paciente novoPaciente = introduzDados();
+        apresentaDados(novoPaciente);
+
+        if (Utils.confirma("Confirma os dados? (S/N)")) {
+            if (hospital.adicionarPaciente(novoPaciente)) {
+                System.out.println("Dados do paciente guardados com sucesso.");
+            } else {
+                System.out.println("Não foi possível guardar os dados do paciente.");
+            }
+        }
+    }
+
+    private Paciente introduzDados() {
+        int id = Utils.readIntFromConsole("Introduza o id do paciente: ");
+        String nome = Utils.readLineFromConsole("Introduza o nome do paciente: ");
+        String sexo = Utils.readLineFromConsole("Introduza o sexo do paciente (M/F): ");
+        Data dataNascimento = Utils.readDateFromConsole("Introduza a data de nascimento (dd-MM-yyyy): ");
+        Data dataInternamento = new Data(); // Pode ser a data atual ou outra lógica
+
+        return new Paciente(id, nome, sexo, dataNascimento, dataInternamento);
+    }
+
+    private void apresentaDados(Paciente paciente) {
+        System.out.println("Paciente: " + paciente.toString());
+    }
+}
+
